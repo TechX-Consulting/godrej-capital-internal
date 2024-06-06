9f7b9b6e2bfcc4516e89f6e61185c9c0e247f697
Lalita <lalita@pixelmechanics.de>
2024-06-06 16:46:54 +0530

    Resolve the lint issues.

--- blocks/loaneligibility/loaneligibility.js
+++ blocks/loaneligibility/loaneligibility.js
@@ -1,17 +1,21 @@
 export default async function decorate(block) {
-    var container = document.querySelector('.loaneligibility'),
-        selectProduct, optionsArray, i, option, applyButton,
-        P, R, N, M, E, pie, line,
-        loan_amt_slider,
-        loan_amt_text,
-        loan_period_text_month,
-        loan_period_slider_month,
-        loan_period_text,
-        loan_period_slider,
-        int_rate_text,
-        int_rate_slider,
-        exisiting_emi_amount_slider,
-        exisiting_emi_text;
+  var container = document.querySelector('.loaneligibility');
+  var selectProduct; 
+  var optionsArray;
+  var i;
+  var option;
+  var applyButton;
+  var P, R, N, M, E, pie, line,
+    loan_amt_slider,
+    loan_amt_text,
+    loan_period_text_month,
+    loan_period_slider_month,
+    loan_period_text,
+    loan_period_slider,
+    int_rate_text,
+    int_rate_slider,
+    exisiting_emi_amount_slider,
+    exisiting_emi_text;
 
     function createElement(type, attributes = {}, ...children) {
         const element = document.createElement(type);
