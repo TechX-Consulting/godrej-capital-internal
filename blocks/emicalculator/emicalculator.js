c8d0988a0d20901edb04568e4082b3fc37751f26
manoj <manoj.kumar@techx-consulting.com>
2024-06-06 12:31:52 +0530

    remove space error in eslint

--- blocks/emicalculator/emicalculator.js
+++ blocks/emicalculator/emicalculator.js
@@ -5,21 +5,18 @@ export default async function decorate() {
     const element = document.createElement(type);
     Object.entries(attributes).forEach(([key, value]) => {
       element.setAttribute(key, value);
-    });
-      
+    });  
     children.forEach((child) => {
       if (typeof child === 'string') {
         element.appendChild(document.createTextNode(child));
       } else {
         element.appendChild(child);
       }
-    });
-      
+    });   
     return element;
   }
 
-  const header = createElement(
-    'div', 
+  const header = createElement('div', 
     { class: 'header' },
     createElement('h1', {}, 'Loan Calculator'),
     createElement('button', {},
