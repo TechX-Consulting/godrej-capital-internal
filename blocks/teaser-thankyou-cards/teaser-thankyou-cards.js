9615e31b8f92dff3233b362f823fbac7bbf4452f
ImjeekxGurjar <jitender.rawat@techx-consulting.com>
2024-06-07 13:34:55 +0530

    become-partner-thankyou-page css and js added

--- blocks/teaser-thankyou-cards/teaser-thankyou-cards.js
+++ blocks/teaser-thankyou-cards/teaser-thankyou-cards.js
@@ -6,18 +6,18 @@ export default function decorate(block) {
     // add teaser class for each entry
     elem.classList.add('teaser');
     if (elem.querySelector('.cta-list')) {
-        elem.classList.add('with-cta-list');
+      elem.classList.add('with-cta-list');
     }
     // give p containing the image a specific class
     const picture = elem.querySelector('picture');
     if (picture && picture.closest('p')) {
-        picture.closest('p').classList.add('image');
+      picture.closest('p').classList.add('image');
     }
     elem
       .querySelector('p:not(.image, .button-container)')
       ?.classList.add('text');
 
-      // add class to all p elements immediately after any heading (h1 to h6)
+    // add class to all p elements immediately after any heading (h1 to h6)
     const headings = elem.querySelectorAll('h1, h2, h3, h4, h5, h6');
     headings.forEach((heading) => {
       let nextElem = heading.nextElementSibling;
@@ -30,7 +30,7 @@ export default function decorate(block) {
     // give cta's link(s) a specific class name
     const ctaLinks = elem.querySelectorAll('.button-container a.button');
     ctaLinks.forEach((cta) => {
-        cta.classList.add('cta');
-      });
+      cta.classList.add('cta');
+    });
   }));
 }
