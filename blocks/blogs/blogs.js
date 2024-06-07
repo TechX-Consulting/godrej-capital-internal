24506cd76f66c767616d4987a05cfc62cea6abc0
Lalita <lalita@pixelmechanics.de>
2024-06-07 12:13:18 +0530

    Resolve the lint issues.

--- blocks/blogs/blogs.js
+++ blocks/blogs/blogs.js
@@ -1,213 +1,213 @@
 export default async function decorate(block) {
-    console.log(block);
-    const mainContainer = document.createElement('div');
-    mainContainer.className = 'container';
-    block.appendChild(mainContainer);
- 
-    // Create input field
-    const inputField = document.createElement('input');
-    inputField.type = 'text';
-    inputField.className = 'input-field';
-    inputField.placeholder = 'Enter text';
-    mainContainer.appendChild(inputField);
- 
-    try {
-        const responseData = await fetch('https://main--eds--aryanjha12.hlx.page/blogs/query-index.json');
-        const dataObj = await responseData.json();
-        console.log('new object is', dataObj);
- 
-        // Store the original data
-        const originalData = dataObj.data;
-        console.log(originalData);
-        let dateDropdown = ['latest to oldest', 'oldest to latest'];
-        // Create and populate dropdowns
-        const dropdown1 = createDropdown(mainContainer, 'date-dropdown', 'Select filters', dateDropdown);
-        const dropdown2 = createDropdown(mainContainer, 'product_dropdown', 'Select category', originalData.map(item => item.category));
- 
-        const cardContainer = document.createElement('div');
-        cardContainer.className = 'card-container';
-        mainContainer.appendChild(cardContainer);
- 
-        const noResultsContainer = document.createElement('div');
-        noResultsContainer.className = 'no-results';
-        noResultsContainer.textContent = 'No results found';
-        noResultsContainer.style.display = 'none'; // Initially hidden
-        mainContainer.appendChild(noResultsContainer);
- 
-        const paginationContainer = document.createElement('div');
-        paginationContainer.className = 'pagination';
-        mainContainer.appendChild(paginationContainer);
- 
-        const itemsPerPage = 2;
-        let currentPage = 1;
- 
-        // Function to create dropdowns
-        function createDropdown(parent, className, placeholderText, optionsArray) {
-            const dropdown = document.createElement('select');
-            dropdown.className = className;
- 
-            const placeholderOption = document.createElement('option');
-            placeholderOption.textContent = placeholderText;
-            placeholderOption.disabled = true;
-            placeholderOption.selected = true;
-            dropdown.appendChild(placeholderOption);
- 
-            optionsArray.forEach(optionText => {
-                const option = document.createElement('option');
-                option.value = optionText.toLowerCase().replace(/ /g, '');
-                option.textContent = optionText;
-                dropdown.appendChild(option);
-            });
- 
-            parent.appendChild(dropdown);
-            return dropdown;
-        }
- 
-        // Function to create cards
-        function createCards(data) {
-            cardContainer.innerHTML = '';
-            data.forEach(entry => {
-                const cardLink = document.createElement('a');
-                cardLink.href = entry.path;
-                cardLink.className = 'card-link';
- 
-                const card = document.createElement('div');
-                card.className = 'card';
- 
-                const image = document.createElement('img');
-                image.src = entry.image;
-                card.appendChild(image);
- 
-                const description = document.createElement('p');
-                description.textContent = entry.description;
-                card.appendChild(description);
- 
-                const readTime = document.createElement('p');
-                readTime.textContent = entry.readtime;
-                card.appendChild(readTime);
- 
-                const date = document.createElement('p');
-                date.textContent = entry.publishdate;
-                card.appendChild(date);
- 
-                cardLink.appendChild(card);
-                cardContainer.appendChild(cardLink);
-            });
-        }
- 
-        function displayData(page, data) {
-            const start = (page - 1) * itemsPerPage;
-            const end = start + itemsPerPage;
-            const pageData = data.slice(start, end);
-            createCards(pageData);
-        }
- 
-        function displayPagination(data) {
-            paginationContainer.innerHTML = '';
-            const totalPages = Math.ceil(data.length / itemsPerPage);
- 
-            const prevButton = document.createElement('button');
-            prevButton.textContent = 'Previous';
-            prevButton.className = 'page-button';
-            prevButton.disabled = currentPage === 1;
-            prevButton.addEventListener('click', function () {
-                if (currentPage > 1) {
-                    currentPage--;
-                    updatePagination(data);
-                }
-            });
-            paginationContainer.appendChild(prevButton);
- 
-            for (let i = 1; i <= totalPages; i++) {
-                const button = document.createElement('button');
-                button.textContent = i;
-                button.className = 'page-button';
-                if (i === currentPage) {
-                    button.classList.add('active');
-                }
-                button.addEventListener('click', () => {
-                    currentPage = i;
-                    updatePagination(data);
-                });
-                paginationContainer.appendChild(button);
-            }
- 
-            const nextButton = document.createElement('button');
-            nextButton.textContent = 'Next';
-            nextButton.className = 'page-button';
-            nextButton.disabled = currentPage === totalPages;
-            nextButton.addEventListener('click', () => {
-                if (currentPage < totalPages) {
-                    currentPage++;
-                    updatePagination(data);
-                }
-            });
-            paginationContainer.appendChild(nextButton);
-        }
- 
-        function updatePagination(data) {
-            displayData(currentPage, data);
-            displayPagination(data);
-        }
- 
-        // Initial cards creation and pagination display
-        updatePagination(originalData);
- 
-        // Filter and sort cards based on dropdown selections and input field
-        dropdown2.addEventListener('change', () => {
-            filterAndSortCards();
-        });
- 
-        dropdown1.addEventListener('change', () => {
-            filterAndSortCards();
-        });
- 
-        inputField.addEventListener('input', () => {
-            const searchText = inputField.value.trim().toLowerCase();
-            // Trigger search only if the length of the search text is at least three characters
-            if (searchText.length >= 3) {
-                filterAndSortCards(searchText);
-            } else if (searchText.length === 0) {
-                // Reset search and show all results if search text is empty
-                filterAndSortCards('');
-            }
-        });
-       
- 
-        function filterAndSortCards() {
-            const selectedCategory = dropdown2.value.toLowerCase().replace(/ /g, '');
-            const selectedFilter = dropdown1.value;
-            const searchText = inputField.value.toLowerCase();
- 
-            // Filter the original data based on the selected category
-            let filteredData = originalData.filter(entry => {
-                const matchesCategory = entry.category.toLowerCase().replace(/ /g, '') === selectedCategory || selectedCategory === 'selectcategory';
-                return matchesCategory;
-            });
- 
-            // Sort the filtered data by date
-            if (selectedFilter === 'oldesttolatest') {
-                filteredData.sort((a, b) => new Date(a.publishdate) - new Date(b.publishdate));
-            } else if (selectedFilter === 'latesttooldest') {
-                filteredData.sort((a, b) => new Date(b.publishdate) - new Date(a.publishdate));
-            }
- 
-            // Further filter the sorted data by search text
-            filteredData = filteredData.filter(entry => entry.description.toLowerCase().includes(searchText));
- 
-            // Show or hide "No results found" message
-            if (filteredData.length === 0) {
-                noResultsContainer.style.display = 'block';
-                paginationContainer.style.display = 'none';
-            } else {
-                noResultsContainer.style.display = 'none';
-                paginationContainer.style.display = 'block';
-            }
-            // Create the cards with the filtered and sorted data
-            currentPage = 1; // Reset to first page on new filter/sort
-            updatePagination(filteredData);
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
+  console.log(block);
+  const mainContainer = document.createElement('div');
+  mainContainer.className = 'container';
+  block.appendChild(mainContainer);
+
+  // Create input field
+  const inputField = document.createElement('input');
+  inputField.type = 'text';
+  inputField.className = 'input-field';
+  inputField.placeholder = 'Enter text';
+  mainContainer.appendChild(inputField);
+
+  try {
+    const responseData = await fetch('https://main--eds--aryanjha12.hlx.page/blogs/query-index.json');
+    const dataObj = await responseData.json();
+    console.log('new object is', dataObj);
+
+    // Store the original data
+    const originalData = dataObj.data;
+    console.log(originalData);
+    let dateDropdown = ['latest to oldest', 'oldest to latest'];
+    // Create and populate dropdowns
+    const dropdown1 = createDropdown(mainContainer, 'date-dropdown', 'Select filters', dateDropdown);
+    const dropdown2 = createDropdown(mainContainer, 'product_dropdown', 'Select category', originalData.map(item => item.category));
+
+    const cardContainer = document.createElement('div');
+    cardContainer.className = 'card-container';
+    mainContainer.appendChild(cardContainer);
+
+    const noResultsContainer = document.createElement('div');
+    noResultsContainer.className = 'no-results';
+    noResultsContainer.textContent = 'No results found';
+    noResultsContainer.style.display = 'none'; // Initially hidden
+    mainContainer.appendChild(noResultsContainer);
+
+    const paginationContainer = document.createElement('div');
+    paginationContainer.className = 'pagination';
+    mainContainer.appendChild(paginationContainer);
+
+    const itemsPerPage = 2;
+    let currentPage = 1;
+
+    // Function to create dropdowns
+    function createDropdown(parent, className, placeholderText, optionsArray) {
+      const dropdown = document.createElement('select');
+      dropdown.className = className;
+
+      const placeholderOption = document.createElement('option');
+      placeholderOption.textContent = placeholderText;
+      placeholderOption.disabled = true;
+      placeholderOption.selected = true;
+      dropdown.appendChild(placeholderOption);
+
+      optionsArray.forEach(optionText => {
+        const option = document.createElement('option');
+        option.value = optionText.toLowerCase().replace(/ /g, '');
+        option.textContent = optionText;
+        dropdown.appendChild(option);
+      });
+
+      parent.appendChild(dropdown);
+      return dropdown;
     }
+
+    // Function to create cards
+    function createCards(data) {
+      cardContainer.innerHTML = '';
+      data.forEach(entry => {
+        const cardLink = document.createElement('a');
+        cardLink.href = entry.path;
+        cardLink.className = 'card-link';
+
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const image = document.createElement('img');
+        image.src = entry.image;
+        card.appendChild(image);
+
+        const description = document.createElement('p');
+        description.textContent = entry.description;
+        card.appendChild(description);
+
+        const readTime = document.createElement('p');
+        readTime.textContent = entry.readtime;
+        card.appendChild(readTime);
+
+        const date = document.createElement('p');
+        date.textContent = entry.publishdate;
+        card.appendChild(date);
+
+        cardLink.appendChild(card);
+        cardContainer.appendChild(cardLink);
+      });
+    }
+
+    function displayData(page, data) {
+      const start = (page - 1) * itemsPerPage;
+      const end = start + itemsPerPage;
+      const pageData = data.slice(start, end);
+      createCards(pageData);
+    }
+
+    function displayPagination(data) {
+      paginationContainer.innerHTML = '';
+      const totalPages = Math.ceil(data.length / itemsPerPage);
+
+      const prevButton = document.createElement('button');
+      prevButton.textContent = 'Previous';
+      prevButton.className = 'page-button';
+      prevButton.disabled = currentPage === 1;
+      prevButton.addEventListener('click', function () {
+        if (currentPage > 1) {
+          currentPage--;
+          updatePagination(data);
+        }
+      });
+      paginationContainer.appendChild(prevButton);
+
+      for (let i = 1; i <= totalPages; i++) {
+        const button = document.createElement('button');
+        button.textContent = i;
+        button.className = 'page-button';
+        if (i === currentPage) {
+          button.classList.add('active');
+        }
+        button.addEventListener('click', () => {
+          currentPage = i;
+          updatePagination(data);
+        });
+        paginationContainer.appendChild(button);
+      }
+
+      const nextButton = document.createElement('button');
+      nextButton.textContent = 'Next';
+      nextButton.className = 'page-button';
+      nextButton.disabled = currentPage === totalPages;
+      nextButton.addEventListener('click', () => {
+        if (currentPage < totalPages) {
+          currentPage++;
+          updatePagination(data);
+        }
+      });
+      paginationContainer.appendChild(nextButton);
+    }
+
+    function updatePagination(data) {
+      displayData(currentPage, data);
+      displayPagination(data);
+    }
+
+    // Initial cards creation and pagination display
+    updatePagination(originalData);
+
+    // Filter and sort cards based on dropdown selections and input field
+    dropdown2.addEventListener('change', () => {
+      filterAndSortCards();
+    });
+
+    dropdown1.addEventListener('change', () => {
+      filterAndSortCards();
+    });
+
+    inputField.addEventListener('input', () => {
+      const searchText = inputField.value.trim().toLowerCase();
+      // Trigger search only if the length of the search text is at least three characters
+      if (searchText.length >= 3) {
+        filterAndSortCards(searchText);
+      } else if (searchText.length === 0) {
+        // Reset search and show all results if search text is empty
+        filterAndSortCards('');
+      }
+    });
+
+
+    function filterAndSortCards() {
+      const selectedCategory = dropdown2.value.toLowerCase().replace(/ /g, '');
+      const selectedFilter = dropdown1.value;
+      const searchText = inputField.value.toLowerCase();
+
+      // Filter the original data based on the selected category
+      let filteredData = originalData.filter(entry => {
+        const matchesCategory = entry.category.toLowerCase().replace(/ /g, '') === selectedCategory || selectedCategory === 'selectcategory';
+        return matchesCategory;
+      });
+
+      // Sort the filtered data by date
+      if (selectedFilter === 'oldesttolatest') {
+        filteredData.sort((a, b) => new Date(a.publishdate) - new Date(b.publishdate));
+      } else if (selectedFilter === 'latesttooldest') {
+        filteredData.sort((a, b) => new Date(b.publishdate) - new Date(a.publishdate));
+      }
+
+      // Further filter the sorted data by search text
+      filteredData = filteredData.filter(entry => entry.description.toLowerCase().includes(searchText));
+
+      // Show or hide "No results found" message
+      if (filteredData.length === 0) {
+        noResultsContainer.style.display = 'block';
+        paginationContainer.style.display = 'none';
+      } else {
+        noResultsContainer.style.display = 'none';
+        paginationContainer.style.display = 'block';
+      }
+      // Create the cards with the filtered and sorted data
+      currentPage = 1; // Reset to first page on new filter/sort
+      updatePagination(filteredData);
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
