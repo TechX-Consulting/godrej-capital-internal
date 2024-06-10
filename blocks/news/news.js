cf5aa64c7a6659b84e9f8167bbb232480b528136
Shivam <gargshivam20345@gmail.com>
2024-06-10 18:10:00 +0530

    This is updated helix-query.yaml file for index-configuration of news and press-release

--- blocks/news/news.js
+++ blocks/news/news.js
@@ -114,14 +114,6 @@ export default async function decorate(block) {
       });
     }
   }
-  // Function to render items on the current page
-  function renderPage() {
-    const start = (currentPage - 1) * itemsPerPage;
-    const end = start + itemsPerPage;
-    const currentData = responseData.slice(start, end);
-    getResponseData(currentData);
-    renderPagination();
-  }
   // Function to render pagination buttons
   function renderPagination() {
     paginationContainer.innerHTML = '';
@@ -144,6 +136,15 @@ export default async function decorate(block) {
       }
     }
   }
+  // Function to render items on the current page
+  function renderPage() {
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const currentData = responseData.slice(start, end);
+    getResponseData(currentData);
+    renderPagination();
+  }
+
 
 
   // Function to sort data based on the selected option
