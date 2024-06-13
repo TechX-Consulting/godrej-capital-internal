d95966fa611dabb96cabdc242fafeb4c634fb142
Ankit saha <sahankit.0722@gmail.com>
2024-06-13 12:32:50 +0530

    media gallery component

--- blocks/media-gallery/media-gallery.js
+++ blocks/media-gallery/media-gallery.js
@@ -301,33 +301,33 @@ async function fetchAndDisplayHtml(url, container, categoryDiv) {
   album.appendChild(nextButton);
 }
 
-function createPictureCardsOnClick(data, container, fullData) {
-  const categoryDiv = createAndAppendElement(container, 'div', {
-    class: 'category-div',
-  });
+// function createPictureCardsOnClick(data, container, fullData) {
+//   const categoryDiv = createAndAppendElement(container, 'div', {
+//     class: 'category-div',
+//   });
 
-  const goBackButton = createAndAppendElement(categoryDiv, 'button', {
-    class: 'go-back-button',
-  });
-  goBackButton.textContent = 'Go Back';
+//   const goBackButton = createAndAppendElement(categoryDiv, 'button', {
+//     class: 'go-back-button',
+//   });
+//   goBackButton.textContent = 'Go Back';
 
-  const categoryHeading = createAndAppendElement(categoryDiv, 'h5', {
-    class: 'category-heading',
-  });
-  categoryHeading.textContent = data.category.toUpperCase();
+//   const categoryHeading = createAndAppendElement(categoryDiv, 'h5', {
+//     class: 'category-heading',
+//   });
+//   categoryHeading.textContent = data.category.toUpperCase();
 
-  goBackButton.addEventListener('click', () => {
-    container.innerHTML = '';
-    categoryDiv.remove();
-    createPictureCards(fullData, container);
-  });
+//   goBackButton.addEventListener('click', () => {
+//     container.innerHTML = '';
+//     categoryDiv.remove();
+//     createPictureCards(fullData, container);
+//   });
 
-  const cardContainer = createAndAppendElement(container, 'div', {
-    class: 'main-card-div',
-    style: 'display: flex;',
-  });
-  fetchAndDisplayHtml(data.album_doc, cardContainer, categoryDiv);
-}
+//   const cardContainer = createAndAppendElement(container, 'div', {
+//     class: 'main-card-div',
+//     style: 'display: flex;',
+//   });
+//   fetchAndDisplayHtml(data.album_doc, cardContainer, categoryDiv);
+// }
 
 function createPictureCards(data, container) {
   data.forEach((item) => {
@@ -344,7 +344,32 @@ function createPictureCards(data, container) {
 
     card.addEventListener('click', async () => {
       container.innerHTML = '';
-      createPictureCardsOnClick(item, container, data);
+      //createPictureCardsOnClick(item, container, data);
+      const categoryDiv = createAndAppendElement(container, 'div', {
+        class: 'category-div',
+      });
+    
+      const goBackButton = createAndAppendElement(categoryDiv, 'button', {
+        class: 'go-back-button',
+      });
+      goBackButton.textContent = 'Go Back';
+    
+      const categoryHeading = createAndAppendElement(categoryDiv, 'h5', {
+        class: 'category-heading',
+      });
+      categoryHeading.textContent = item.category.toUpperCase();
+    
+      goBackButton.addEventListener('click', () => {
+        container.innerHTML = '';
+        categoryDiv.remove();
+        createPictureCards(data, container);
+      });
+    
+      const cardContainer = createAndAppendElement(container, 'div', {
+        class: 'main-card-div',
+        style: 'display: flex;',
+      });
+      fetchAndDisplayHtml(item.album_doc, cardContainer, categoryDiv);
     });
   });
 }
