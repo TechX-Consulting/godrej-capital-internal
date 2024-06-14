bafaded3db7881a2c91d8116bc7fd467d8aac021
Ankit saha <sahankit.0722@gmail.com>
2024-06-14 15:11:15 +0530

    media gallery component

--- tools/sidekick/plugins/utils/utils.js
+++ tools/sidekick/plugins/utils/utils.js
@@ -1,11 +1,11 @@
 /*
  * Copyright 2023 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the 'License');
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -32,7 +32,6 @@ export function createElement(tagName, classes, props) {
     const classesArr = (typeof classes === 'string') ? [classes] : classes;
     elem.classList.add(...classesArr);
   }
-
   if (props) {
     Object.keys(props).forEach((propName) => {
       elem.setAttribute(propName, props[propName]);
@@ -43,9 +42,9 @@ export function createElement(tagName, classes, props) {
 }
 
 /**
- * Copies to the clipboard
- * @param {Blob} blob The data
- */
+   * Copies to the clipboard
+   * @param {Blob} blob The data
+   */
 export function createCopy(blob) {
   const data = [new ClipboardItem({ [blob.type]: blob })];
   navigator.clipboard.write(data);
