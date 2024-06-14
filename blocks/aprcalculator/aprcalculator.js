836a6a2e79865039af5417ada4a3d03035377086
manoj <manoj.kumar@techx-consulting.com>
2024-06-14 15:16:34 +0530

    add the logic of aprcalculator

--- blocks/aprcalculator/aprcalculator.js
+++ blocks/aprcalculator/aprcalculator.js
@@ -180,13 +180,10 @@ export default async function decorate() {
 
   window.updateDisplay = updateDisplay;
   window.updateRange = function (id) {
-
-    const {value} = document.getElementById(`${id}Range`).value;
+    const { value } = document.getElementById(`${id}Range`).value;
     document.getElementById(id).textContent = value;
     updateDisplay();
-    
   };
-
   document.getElementById('loanamount').addEventListener('input', function () {
     // const value = parseFloat(this.textContent.replace(/[^\d.]/g, ''));
     // const min = parseFloat(this.dataset.min);
