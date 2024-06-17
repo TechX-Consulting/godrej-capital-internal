60ca94b3497fbb137024418bb4ba021aaaf8935d
Shivam <gargshivam20345@gmail.com>
2024-06-17 18:04:29 +0530

    This is updated helix-query.yaml file for index-configuration of news and press-release

--- blocks/custom-tab/custom-tab.js
+++ blocks/custom-tab/custom-tab.js
@@ -25,7 +25,6 @@ function renderData(data, selectedTab, selectedOption, tabpanel) {
   );
 
   if (filteredData.length === 0) {
-    console.error(`Data for combination '${selectedTab} - ${selectedOption}' not found.`);
     return;
   }
 
@@ -33,7 +32,7 @@ function renderData(data, selectedTab, selectedOption, tabpanel) {
   tabpanel.innerHTML = '';
 
   // Display the filtered data
-  filteredData.forEach(item => {
+  filteredData.forEach((item) => {
     let sectionIndex = 1;
 
     // Iterate through sections until no more titles are found
@@ -58,13 +57,13 @@ function renderData(data, selectedTab, selectedOption, tabpanel) {
         const bulletPointsList = bulletPoints
           .split('\n')
           .map(bp => bp.trim())
-          .filter(bp => bp !== '');
+          .filter((bp) => bp !== '');
 
         if (bulletPointsList.length > 0) {
           const listElement = document.createElement('ul');
           listElement.style.listStyleType = 'disc'; // Set list style to bullet points
 
-          bulletPointsList.forEach(bullet => {
+          bulletPointsList.forEach((bullet) => {
             const listItem = document.createElement('li');
             listItem.textContent = bullet;
             listElement.appendChild(listItem);
@@ -227,7 +226,6 @@ async function decorate(block) {
   data = await fetchData(apiUrl);
 
   if (!data) {
-    console.error('Data not available.');
     return;
   }
 
